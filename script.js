50c9309b147ea07717010109854e1d91d6a31f1f
OmegaGinga <abdielflores980401@hotmail.com>
2024-07-24 14:10:04 -0600

    Project finished
    Added random colors with opacity
    Opacity increments every time mouse hover

--- script.js
+++ script.js
@@ -4,18 +4,26 @@ function createGrid(size) {
     container.innerHTML = '';
     for (let i = 0; i < size * size; i++) {
         let div = document.createElement('div');
+        div.style.border = '1px solid gray';
+        div.style.boxSizing = 'border-box';
+        div.style.flex = `1 0 calc(100% / ${size})`;
+        div.style.height = `calc(100% / ${size})`;
+        div.style.opacity = '0.1';
+        div.dataset.opacity = '0.1';
         container.appendChild(div);
     }
 
     let grids = document.querySelectorAll('.container div');
 
     grids.forEach((grid) => {
-        grid.style.border = '1px solid gray';
-        grid.style.boxSizing = 'border-box';
-        grid.style.flex = `1 0 calc(100% / ${size})`;
-        grid.style.height = `calc(100% / ${size})`;
         grid.addEventListener('mouseover', () => {
-            grid.style.backgroundColor = `rgb(${Math.floor(Math.random()*255+1)},${Math.floor(Math.random()*255+1)},${Math.floor(Math.random()*255+1)})`;
+            let currentOpacity = parseFloat(grid.dataset.opacity);
+            if (currentOpacity < 1.0) {
+                let newOpacity = currentOpacity + 0.1;
+                grid.style.backgroundColor = `rgba(${Math.floor(Math.random() * 255 + 1)}, ${Math.floor(Math.random() * 255 + 1)}, ${Math.floor(Math.random() * 255 + 1)}, ${newOpacity})`;
+                grid.style.opacity = newOpacity;
+                grid.dataset.opacity = newOpacity;
+            }
         });
     });
 }
